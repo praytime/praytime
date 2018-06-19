e0239c2bec03eeb276f859f408380658fb922a92
theimpostor <theimpostor.gkasdf@mailhero.io>
2018-06-19 18:36:43 -0500

    WIP get resultsUrl

--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-curl -v -v --include --request POST --header "Content-Type: application/json" --data-binary "$(./post-data.sh)" https://api.apify.com/v1/hMf69fRQCWWX84d5Z/crawlers/bBG5kmupgKJPssdeh/execute?token=ptgJXnS5JYaRCHG8f6RAx5oHM
+# TODO: set trap
+
+set -euf -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+. "${SCRIPT_DIR}/../.env"
+
+resultsUrl="$(curl \
+    -fsSL \
+    --request POST \
+    --header "Content-Type: application/json" \
+    --data-binary "$(./post-data.sh)" \
+    https://api.apify.com/v1/hMf69fRQCWWX84d5Z/crawlers/bBG5kmupgKJPssdeh/execute?token="${APIFY_TOKEN}" \
+    | jq --raw-output '.resultsUrl')"  
+    
+echo "resultsUrl is $resultsUrl"
+
+sleep 10
+
+curl -v -v "$resultsUrl"
+    
+#    | jq '.[].pageFunctionResult'
+
