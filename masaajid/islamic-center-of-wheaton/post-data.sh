5adb2f1ac1c9d66484617a130c47f6ecf841f017
theimpostor <theimpostor.gkasdf@mailhero.io>
2018-07-12 08:50:08 -0500

    add icw

---
+++ masaajid/islamic-center-of-wheaton/post-data.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cat <<EOF
+{
+  "_id": "bBG5kmupgKJPssdeh",
+  "startUrls": [ { "value": "https://www.icwonline.org/" } ],
+  "pageFunction": $(python -c 'import json,sys; print(json.dumps(sys.stdin.read()))' < page-function.js)
+}
+EOF
+
+
