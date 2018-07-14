50e63f56e2a2362dac848bb56bf851be6366ba7d
theimpostor <theimpostor.gkasdf@mailhero.io>
2018-07-14 11:32:18 -0500

    add delay between masaajid

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-const masaajid = [
-//    './lib/islamic-center-of-naperville',
-    './lib/masjid-darussalam'
-]
-
 const ApifyClient = require('apify-client')
 const apifyClient = new ApifyClient({
     userId: process.env.APIFY_USER_ID,
     token: process.env.APIFY_TOKEN
 })
-
 const lib = require('./lib')
 
-masaajid.forEach((m) => {
-    lib.getPagefunctionResults(apifyClient, require(m).settings, (r) => {
+const masaajid = [
+    './lib/islamic-center-of-naperville',
+    './lib/islamic-center-of-romeoville',
+    './lib/islamic-center-of-wheaton',
+    './lib/masjid-darussalam',
+]
+
+let masjidIdx = 0
+
+function run() {
+    let masjid = masaajid[masjidIdx++]
+    console.error("starting %s", masjid)
+    lib.getPagefunctionResults(apifyClient, require(masjid).settings, (r) => {
         console.log("%j", r)
     })
-})
+    if (masjidIdx < masaajid.length) {
+        // delay before starting next
+        setTimeout(run, 20000)
+    }
+}
+
+// kick off loop
+run()
