71af4cc64492ab20307b35c29377369bea1aeb0a
theimpostor <theimpostor.gkasdf@mailhero.io>
2018-07-21 23:09:09 -0500

    format with standardjs

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
 const ApifyClient = require('apify-client')
 const apifyClient = new ApifyClient({
-    userId: process.env.APIFY_USER_ID,
-    token: process.env.APIFY_TOKEN
+  userId: process.env.APIFY_USER_ID,
+  token: process.env.APIFY_TOKEN
 })
 const lib = require('./lib')
 
-
 let masaajid = [
-    './lib/fox-valley-muslim-community-center',
-    './lib/islamic-center-of-naperville',
-    './lib/islamic-center-of-romeoville',
-    './lib/islamic-center-of-wheaton',
-    './lib/islamic-foundation-of-southwest-suburbs',
-    './lib/islamic-foundation-of-villa-park',
-    './lib/madani-masjid-westmont',
-    './lib/masjid-al-mustafa-westmont',
-    './lib/masjid-darussalam',
-    './lib/masjid-haqq-lombard',
-    './lib/mcc-chicago',
-    './lib/mecca-center',
-    './lib/muslim-association-of-bolingbrook',
-    './lib/muslim-society-inc-bloomingdale',
+  './lib/fox-valley-muslim-community-center',
+  './lib/islamic-center-of-naperville',
+  './lib/islamic-center-of-romeoville',
+  './lib/islamic-center-of-wheaton',
+  './lib/islamic-foundation-of-southwest-suburbs',
+  './lib/islamic-foundation-of-villa-park',
+  './lib/madani-masjid-westmont',
+  './lib/masjid-al-mustafa-westmont',
+  './lib/masjid-darussalam',
+  './lib/masjid-haqq-lombard',
+  './lib/mcc-chicago',
+  './lib/mecca-center',
+  './lib/muslim-association-of-bolingbrook',
+  './lib/muslim-society-inc-bloomingdale'
 ]
 
 const main = async () => {
-    for (const masjid of masaajid) {
-        try {
-            console.error("starting %s", masjid)
+  for (const masjid of masaajid) {
+    try {
+      console.error('starting %s', masjid)
 
-            const masjidLib = require(masjid)
+      const masjidLib = require(masjid)
 
-            let results = {}
-            if (masjidLib.settings) {
-                results = await lib.getPagefunctionResults(apifyClient, masjidLib.settings)
-            } else if (masjidLib.run) {
-                results = await masjidLib.run()
-            }
+      let results = {}
+      if (masjidLib.settings) {
+        results = await lib.getPagefunctionResults(apifyClient, masjidLib.settings)
+      } else if (masjidLib.run) {
+        results = await masjidLib.run()
+      }
 
-            results.forEach((r) => {
-                console.log("%j", r)
-            });
-        } catch (err) {
-            console.error(err)
-        }
+      results.forEach((r) => {
+        console.log('%j', r)
+      })
+    } catch (err) {
+      console.error(err)
     }
+  }
 }
 
-const argv = process.argv.slice(2);
+const argv = process.argv.slice(2)
 
 if (argv.length > 0) {
-    masaajid = argv
-} 
+  masaajid = argv
+}
 
 main()
--- research/build-actor/source.js
+++ research/build-actor/source.js
@@ -1,29 +1,28 @@
 const Apify = require('apify')
-const actId = "bd84eHbgNMi28u9bH"
+const actId = 'bd84eHbgNMi28u9bH'
 
 Apify.main(async () => {
-    const apifyClient = Apify.client
+  const apifyClient = Apify.client
 
-    console.log("building act")
-    let actBuild = await apifyClient.acts.buildAct({
-        actId: actId,
-        useCache: true,
-        waitForFinish: 0,
-        version: "0.0"
+  console.log('building act')
+  let actBuild = await apifyClient.acts.buildAct({
+    actId: actId,
+    useCache: true,
+    waitForFinish: 0,
+    version: '0.0'
+  })
+  let buildId = actBuild.id
+
+  console.log(actBuild)
+
+  while (actBuild.status == 'RUNNING') {
+    console.log('Getting status of %s', buildId)
+    actBuild = await apifyClient.acts.getBuild({
+      buildId: buildId,
+      waitForFinish: 10
     })
-    let buildId = actBuild.id
-    
-    console.log(actBuild)
-    
-    while (actBuild.status == "RUNNING") {
-        console.log("Getting status of %s", buildId)
-        actBuild = await apifyClient.acts.getBuild({
-            buildId: buildId,
-            waitForFinish: 10
-        })
-    }
+  }
 
-    console.log("completed")
-    console.log(actBuild)
-
+  console.log('completed')
+  console.log(actBuild)
 })
