aa7fc6745cd581533e737103a93bc523ff517f40
theimpostor <sahirhoda@gmail.com>
2020-02-17 12:32:14 -0600

    New directory naming structure, following lib/COUNTRY/STATE/mosque

---
+++ script/mv.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# helper script to change directory name under lib and update index.js
+
+set -eu -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_DIR="${SCRIPT_DIR}/.."
+
+# shellcheck source=/dev/null
+. "${PROJECT_DIR}/script/common.sh"
+
+trap exiterr EXIT
+
+set -x
+
+git mv "$1" "$2"
+
+perl -p -i -e "s{\Q$1\E}{$2}" index.js 
+
+set +x
+
+trap - EXIT
