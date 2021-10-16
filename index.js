c145e90ba83c1f4b4b4454ff12dda8aeea3606f0
theimpostor <sahirhoda@gmail.com>
2021-10-16 12:27:01 -0500

    add bicc chicago

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let masaajid = [
   './lib/US/IL/aqsa-community-center-plainfield',
   './lib/US/IL/at-takaful-islamic-society-chicago',
   './lib/US/IL/batavia-islamic-center',
+  './lib/US/IL/bicc-chicago',
   './lib/US/IL/chicago-islamic-center',
   './lib/US/IL/chicago-mosque',
   './lib/US/IL/cimic',
