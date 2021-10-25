4d89e55c85a0267dd5c42003d6038d4d92a770f4
theimpostor <sahirhoda@gmail.com>
2021-10-25 08:11:04 -0500

    new google maps text search tool

---
+++ go/cmd/text-search/go.mod
@@ -0,0 +1,15 @@
+module github.com/praytime/praytime/go/cmd/text-search
+
+go 1.17
+
+require (
+	github.com/integralist/go-findroot v0.0.0-20160518114804-ac90681525dc
+	github.com/joho/godotenv v1.4.0
+	googlemaps.github.io/maps v1.3.2
+)
+
+require (
+	github.com/google/uuid v1.1.1 // indirect
+	go.opencensus.io v0.22.3 // indirect
+	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
+)
---
+++ go/cmd/text-search/main.go
@@ -0,0 +1,84 @@
+// does a google maps places text search with type=mosque and prints results that aren't found in lib
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+	// "text/template"
+
+	// "github.com/google/uuid"
+	// "github.com/gosimple/slug"
+	"github.com/integralist/go-findroot/find"
+	"github.com/joho/godotenv"
+	"googlemaps.github.io/maps"
+)
+
+func loadDefaultEnvVars() {
+	if home, err := os.UserHomeDir(); err == nil {
+		envFile := filepath.Join(home, ".godotenv")
+		if stat, err := os.Stat(envFile); err == nil && !stat.IsDir() {
+			if err = godotenv.Load(envFile); err != nil {
+				log.Fatal("Error loading ", envFile, ":", err)
+			}
+		}
+	}
+}
+
+func newGmapsClient() *maps.Client {
+	c, err := maps.NewClient(maps.WithAPIKey(os.Getenv("GMAPS_API_KEY")))
+	if err != nil {
+		log.Fatal("Error creating gmaps client:", err)
+	}
+	return c
+}
+
+func getGitRoot() string {
+	root, err := find.Repo()
+	if err != nil {
+		log.Fatal("Error looking for git root:", err)
+	}
+	return root.Path
+}
+
+func main() {
+
+	flag.Parse()
+
+	loadDefaultEnvVars()
+
+	gmapsClient := newGmapsClient()
+
+	// gitRoot := getGitRoot()
+
+	for _, arg := range flag.Args() {
+		pageToken := ""
+		moreResults := func() bool {
+			return pageToken != ""
+		}
+		for ok := true; ok; ok = moreResults() { // do - while
+			req := &maps.TextSearchRequest{
+				Query:     arg,
+				Radius:    50000,
+				Type:      "mosque",
+				PageToken: pageToken,
+			}
+			resp, err := gmapsClient.TextSearch(context.Background(), req)
+			if err != nil {
+				log.Fatal("Error creating text search request", err)
+			}
+			pageToken = resp.NextPageToken
+			for _, result := range resp.Results {
+				fmt.Printf("https://www.google.com/maps/search/?api=1&query=none&query_place_id=%s\t%s\t%s\n", result.PlaceID, result.PlaceID, result.Name)
+			}
+			if moreResults() {
+				time.Sleep(2 * time.Second)
+			}
+		}
+	}
+}
