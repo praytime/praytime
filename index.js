32522dadb5a7abd17f155904b887d47b3e789624
theimpostor <sahirhoda@gmail.com>
2022-01-07 08:52:58 -0600

    add options to skip static and puppeteer crawlers

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const geofire = require('geofire-common')
 
-let masaajid = []
+// If true will not run puppeteer crawlers but still may output static data
+const puppeteerDisabled = ('PUPPETEER_DISABLED' in process.env)
+// If true will not run or output puppeteer crawler data
+let skipPuppeteer = false
+// If true will not output static data
+let skipStatic = false
 
 const argv = process.argv.slice(2)
+
+for (; argv.length; argv.shift()) {
+  if (!argv[0].startsWith('--')) {
+    break
+  }
+  switch (argv[0]) {
+    case '--skip-static':
+      skipStatic = true
+      break
+    case '--skip-ppt':
+      skipPuppeteer = true
+      break
+    default:
+      console.error(`Unknown argument: ${argv[0]}`)
+      process.exit(1)
+  }
+}
+
+let masaajid = []
 
 if (argv.length > 0) {
   // resolve relative module paths
@@ -22,8 +46,6 @@ if (argv.length > 0) {
   masaajid = lib.masaajid
 }
 
-const puppeteerDisabled = ('PUPPETEER_DISABLED' in process.env)
-
 const main = async () => {
   for (const masjid of masaajid) {
     try {
@@ -33,13 +55,19 @@ const main = async () => {
 
       let results = masjidLib.ids
       if (masjidLib.run) {
-        if (puppeteerDisabled && masjidLib.puppeteer) {
+        if ((puppeteerDisabled || skipPuppeteer) && masjidLib.puppeteer) {
           console.error('skipping puppeteer crawler: %s', masjid)
+          if (skipPuppeteer) {
+            continue
+          }
         } else {
           // generic run function
           results = await masjidLib.run()
         }
-      }
+      } else if (skipStatic) {
+        console.error('skipping static crawler: %s', masjid)
+        continue
+      } // else static crawler
 
       results.forEach((r) => {
         r.crawlTime = new Date()
